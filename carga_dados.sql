dcf2cff0d8032a7d96640a7c9873ed2fec0b603b
Lucas Antonio <lucasklsz1@gmail.com>
2025-11-11 12:33:20 -0300

    fix: Altera nome das colunas

--- carga_dados.sql
+++ carga_dados.sql
@@ -1,6 +1,17 @@
-INSERT INTO Dim_Transporte (Modalidade) VALUES
-('Pie'),
-('Bicicleta'),
-('Caballo'),
-('Silla de ruedas'),
-('Vela');
+UPDATE Dim_Transporte
+SET Modalidade = 'Pé'
+WHERE Modalidade = 'Pie';
+
+UPDATE Dim_Transporte
+SET Modalidade = 'Cavalo'
+WHERE Modalidade = 'Caballo';
+
+UPDATE Dim_Transporte
+SET Modalidade = 'Cadeira de rodas'
+WHERE Modalidade = 'Silla de ruedas';
+
+UPDATE Dim_Transporte
+SET Modalidade = 'Barco'
+WHERE Modalidade = 'Vela';
+
+SELECT * FROM dim_transporte;
